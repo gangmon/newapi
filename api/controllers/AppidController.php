1838161c09566f83a276a036f0d0c761dc2dbaa8
gangmon <facck@sina.com>
2018-06-21 22:05:59 +0800

    template ok

--- api/controllers/AppidController.php
+++ api/controllers/AppidController.php
@@ -134,15 +134,32 @@ class AppidController extends Controller
 
     public function actionTemplate()
     {
+        /*
         $url_token = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wx2f44d41564140f7f&secret=de27bae6891e039c3b67909ddb645b57";
 //        $access_token = $this->getcurl($url_token);
         $access_token = $this->get_access_token();
-        $url = "https://api.weixin.qq.com/cgi-bin/wxopen/template/library/list?access_token=".$access_token;
+        print_r($access_token);
+        */
 
-        $data = array();
-        $data['offset'] = Yii::$app->request->post('offset');
-        $data['count'] = Yii::$app->request->post('count');
-//        print_r($data);
+        $appid = 'wx2f44d41564140f7f';
+        $secret = 'de27bae6891e039c3b67909ddb645b57';
+
+
+        $url_access_token = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid='.$appid.'&secret='.$secret;
+
+        $json_access_token = $this -> sendCmd($url_access_token,array());
+
+        $arr_access_token = json_decode($json_access_token,true);
+
+        $access_token = $arr_access_token['access_token'];
+        $url = "https://api.weixin.qq.com/cgi-bin/wxopen/template/library/list?access_token=".$access_token;
+//
+//        $data = array();
+//        $data['offset'] = Yii::$app->request->post('offset');
+//        $data['count'] = Yii::$app->request->post('count');
+////        print_r($data);
+        $data = '{"offset":0,"count":5}';
+
         $result = $this -> sendCmd($url,$data);
 
         return $result;
